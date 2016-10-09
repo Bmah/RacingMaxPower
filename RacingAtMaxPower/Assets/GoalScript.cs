a277dc6b6f1f971e44f57230561edb82f0c0c9ee
Brian M <bkmah@ucdavis.edu>
2016-10-08 21:12:26 -0700

    MoreIntegration

---
+++ RacingAtMaxPower/Assets/GoalScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+
+	 void OnTriggerEnter(Collider other){
+		if (other.name == "car") {
+			Debug.Log ("You WIN");
+		}
+
+	}
+}
