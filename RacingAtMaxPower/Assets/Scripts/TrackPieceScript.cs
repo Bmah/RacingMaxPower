b91d09589ee165db193f15da5045f5c0f999a614
thecomediclife <thecomediclife324@gmail.com>
2016-10-08 22:46:00 -0700

    Transition Script added

--- RacingAtMaxPower/Assets/Scripts/TrackPieceScript.cs
+++ RacingAtMaxPower/Assets/Scripts/TrackPieceScript.cs
@@ -5,11 +5,33 @@ public class TrackPieceScript : MonoBehaviour {
 
 	public bool isTurn = false;
 
+	private Color startingColor;
+	private bool transitionEvent;
+	private float transitionSpeed = 1f;
+
+	public void Start() {
+		startingColor = GetComponent<Renderer> ().material.color;
+		GetComponent<Renderer> ().material.color = new Color (startingColor.r, startingColor.g, startingColor.b, 0f);
+		transitionEvent = true;
+	}
+
+	public void Update() {
+		if (transitionEvent) {
+			startingColor = GetComponent<Renderer> ().material.color;
+			GetComponent<Renderer> ().material.color = new Color (startingColor.r,startingColor.g, startingColor.b, Mathf.MoveTowards(startingColor.a, 1f, transitionSpeed * Time.deltaTime));
+		} else {
+			startingColor = GetComponent<Renderer> ().material.color;
+			GetComponent<Renderer> ().material.color = new Color (startingColor.r,startingColor.g, startingColor.b, Mathf.MoveTowards(startingColor.a, 0f, transitionSpeed * Time.deltaTime));
+		}
+
+		//Debug.Log(Mathf.MoveTowards(startingColor.a, 1f, 0f * Time.deltaTime));
+	}
+
 	public void Appear(){
-		
+		transitionEvent = true;
 	}
 
 	public void Dissapear(){
-		
+		transitionEvent = false;
 	}
 }
